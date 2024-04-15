cce114bdf2cece358d69adb9fa31f24919271b40
Changjun Yeo <lunaradio24@gmail.com>
2024-04-15 19:09:50 +0900

    수정

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Firebase SDK 라이브러리 가져오기
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-analytics.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 import {
   collection,
@@ -30,25 +29,28 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// 기록하기 버튼 클릭시
+/*******   Create  *******/
+// 추가하기 버튼 클릭시
 $("#postingbtn").click(async function () {
   let doc = {
     image: $("#image").val(),
-    title: $("#title").val(),
-    content: $("#content").val(),
-    date: $("#date").val(),
+    name: $("#name").val(),
+    introduce: $("#introduce").val(),
+    mbti: $("#mbti").val(),
+    time: Timestamp.now(),
   };
-  await addDoc(collection(db, "albums"), doc);
+  await addDoc(collection(db, "members"), doc);
   alert("저장 완료!");
-  window.location.reload();
+  self.close();
+  window.opener.location.reload();
 });
 
 // '멤버 추가' 버튼 클릭시
 let popup_window;
 $("#savebtn").click(async function () {
   popup_window = window.open(
-    "a.html",
-    "name_a",
+    "newcard.html",
+    "_blank",
     "width=500,height=500,left=200,top=200"
   );
 });
@@ -58,39 +60,47 @@ $("#closebtn").click(async function () {
   self.close();
 });
 
-$("#member-image").click(async function () {
-  //   window.open("member.html", "_blank", "width=500,height=500,left=200,top=200");
-  console.log("hi");
-  $(location).attr("href", "member.html");
-});
+/*******   Read  *******/
+let docs = await getDocs(collection(db, "members"), orderBy("time"));
+async function readDocs() {
+  docs.forEach((doc) => {
+    let row = doc.data();
+    let image = row["image"];
+    let name = row["name"];
+    let introduce = row["introduce"];
+    let mbti = row["mbti"];
 
-// Read (데이터 불러오기)
-let docs = await getDocs(collection(db, "albums"));
-docs.forEach((doc) => {
-  let row = doc.data();
+    let temp_html = `
+          <div class="col" id="member-image">
+            <div class="card h-100">
+              <img
+                src="${image}"
+                class="card-img-top"
+              />
+              <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">${introduce}</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-body-secondary">${mbti}</small>
+              </div>
+            </div>
+          </div>`;
+    $("#card").append(temp_html);
+  });
+}
 
-  let image = row["image"];
-  let title = row["title"];
-  let content = row["content"];
-  let date = row["date"];
+/*******   Initiate  *******/
+function readyDoc() {
+  if (document.readyState !== "loading") {
+    //read saved data
+    readDocs();
+  } else {
+    document.addEventListener("DOMContentLoaded");
+  }
+}
+readyDoc();
 
-  let temp_html = `
-    <div class="col">
-      <div class="card h-100">
-        <img
-          src="${image}"
-          class="card-img-top"
-          alt="..."
-          id="member-image"
-        />
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-          <p class="card-text">${content}</p>
-        </div>
-        <div class="card-footer">
-          <small class="text-body-secondary">${date}</small>
-        </div>
-      </div>
-    </div>`;
-  $("#card").append(temp_html);
+$(document).on("click", "#member-image", function () {
+  window.open("member.html", "_blank", "width=500,height=500,left=200,top=200");
 });
