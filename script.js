bc7e88ec231779dac41d6a6c5a743b29f17e613f
diddntjd99 <diddntjd99@gmail.com>
2024-04-15 21:02:56 +0900

    정보 수정하기 창에 데이터 불러오기

--- script.js
+++ script.js
@@ -2,27 +2,27 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 import {
-  collection,
-  addDoc,
-  getDoc,
-  getDocs,
-  updateDoc,
-  deleteDoc,
-  doc,
-  query,
-  orderBy,
-  Timestamp,
+    collection,
+    addDoc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+    doc,
+    query,
+    orderBy,
+    Timestamp,
 } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 // Firebase 구성 정보 설정
 const firebaseConfig = {
-  apiKey: "AIzaSyAv-qdHJTtM3PZleVXA8ZuVlGnHLYjcUJs",
-  authDomain: "teampage-3217c.firebaseapp.com",
-  projectId: "teampage-3217c",
-  storageBucket: "teampage-3217c.appspot.com",
-  messagingSenderId: "853950108531",
-  appId: "1:853950108531:web:0b70afd3d3d71addc2eb06",
-  measurementId: "G-B4741RFE19",
+    apiKey: "AIzaSyAv-qdHJTtM3PZleVXA8ZuVlGnHLYjcUJs",
+    authDomain: "teampage-3217c.firebaseapp.com",
+    projectId: "teampage-3217c",
+    storageBucket: "teampage-3217c.appspot.com",
+    messagingSenderId: "853950108531",
+    appId: "1:853950108531:web:0b70afd3d3d71addc2eb06",
+    measurementId: "G-B4741RFE19",
 };
 
 // Firebase 인스턴스 초기화
@@ -32,49 +32,49 @@ const db = getFirestore(app);
 /*******   Create  *******/
 // 추가하기 버튼 클릭시
 $("#postingbtn").click(async function () {
-  let doc = {
-    image: $("#image").val(),
-    name: $("#name").val(),
-    introduce: $("#introduce").val(),
-    mbti: $("#mbti").val(),
-    time: Timestamp.now(),
-  };
-  await addDoc(collection(db, "members"), doc);
-  alert("저장 완료!");
-  self.close();
-  window.opener.location.reload();
+    let doc = {
+        image: $("#image").val(),
+        name: $("#name").val(),
+        introduce: $("#introduce").val(),
+        mbti: $("#mbti").val(),
+        time: Timestamp.now(),
+    };
+    await addDoc(collection(db, "members"), doc);
+    alert("저장 완료!");
+    self.close();
+    window.opener.location.reload();
 });
 
 // '멤버 추가' 버튼 클릭시
 let popup_window;
 $("#savebtn").click(async function () {
-  popup_window = window.open(
-    "newcard.html",
-    "_blank",
-    "width=500,height=500,left=200,top=200"
-  );
+    popup_window = window.open(
+        "newcard.html",
+        "_blank",
+        "width=500,height=500,left=200,top=200"
+    );
 });
 
 // 팝업창 '닫기' 버튼 클릭시
 $("#closebtn").click(async function () {
-  self.close();
+    self.close();
 });
 
 /*******   Read  *******/
 let docs = await getDocs(
-  //시간순 정렬해서 query로 불러오기
-  query(collection(db, "members"), orderBy("time"))
+    //시간순 정렬해서 query로 불러오기
+    query(collection(db, "members"), orderBy("time"))
 );
 
 async function readDB() {
-  docs.forEach((doc) => {
-    let row = doc.data();
-    let image = row["image"];
-    let name = row["name"];
-    let introduce = row["introduce"];
-    let mbti = row["mbti"];
+    docs.forEach((doc) => {
+        let row = doc.data();
+        let image = row["image"];
+        let name = row["name"];
+        let introduce = row["introduce"];
+        let mbti = row["mbti"];
 
-    let temp_html = `
+        let temp_html = `
           <div class="col">
             <div class="card h-100">
               <img
@@ -95,42 +95,54 @@ async function readDB() {
               </div>
             </div>
           </div>`;
-    $("#card").append(temp_html);
-  });
+        $("#card").append(temp_html);
+    });
 }
 
 /*******   Initiate  *******/
 function readyDoc() {
-  if (document.readyState !== "loading") {
-    //read saved data
-    readDB();
-  } else {
-    document.addEventListener("DOMContentLoaded");
-  }
+    if (document.readyState !== "loading") {
+        //read saved data
+        readDB();
+    } else {
+        document.addEventListener("DOMContentLoaded");
+    }
 }
 readyDoc();
 
 // 이미지 클릭시 새창으로 개인 페이지 열기
 $(document).on("click", "#member-image", function () {
-  window.open("member.html", "_blank", "width=500,height=500,left=200,top=200");
+    window.open("member.html", "_blank", "width=500,height=500,left=200,top=200");
 });
 
 // Edit 버튼 클릭시
 $(document).on("click", ".editBtn", async function () {
-  let docID = this.parentElement.id;
+    let docID = this.parentElement.id;
+    let docData = doc(db, "members", docID);
+    let getData = await getDoc(docData);
 
-  //   let doc = await getDoc(db, "members", docID);
-  let openWin = window.open(
-    "newcard.html",
-    "_blank",
-    "width=500,height=500,left=200,top=200"
-  );
-  openWin.document.getElementById("name").value = docID;
+    let openWin = window.open(
+        "newcard.html",
+        "_blank",
+        "width=500,height=500,left=200,top=200"
+    );
+
+    console.log(openWin.document.getElementById("name"));
+    console.log(getData.data()["name"]);
+
+    openWin.onload = function () {
+        openWin.document.getElementById("image").value = getData.data()["image"];
+        openWin.document.getElementById("name").value = getData.data()["name"];
+        openWin.document.getElementById("introduce").value = getData.data()["introduce"];
+        openWin.document.getElementById("mbti").value = getData.data()["mbti"];
+    }
+
+
 });
 
 // Delete 버튼 클릭시
 $(document).on("click", ".delBtn", async function () {
-  let docID = this.parentElement.id;
-  await deleteDoc(doc(db, "members", docID));
-  window.location.reload();
+    let docID = this.parentElement.id;
+    await deleteDoc(doc(db, "members", docID));
+    window.location.reload();
 });
