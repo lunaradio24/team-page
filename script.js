5e671516dcfafb2f343bda6c90fa0c4a18586aac
diddntjd99 <diddntjd99@gmail.com>
2024-04-16 18:19:10 +0900

    nav bar 기능 추가, 첫 화면만 하드코딩 식 호출 필요

--- script.js
+++ script.js
@@ -2,27 +2,27 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 import {
-  collection,
-  addDoc,
-  getDoc,
-  getDocs,
-  updateDoc,
-  deleteDoc,
-  doc,
-  query,
-  orderBy,
-  Timestamp,
+    collection,
+    addDoc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+    doc,
+    query,
+    orderBy,
+    Timestamp,
 } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 // Firebase 구성 정보 설정
 const firebaseConfig = {
-  apiKey: "AIzaSyAv-qdHJTtM3PZleVXA8ZuVlGnHLYjcUJs",
-  authDomain: "teampage-3217c.firebaseapp.com",
-  projectId: "teampage-3217c",
-  storageBucket: "teampage-3217c.appspot.com",
-  messagingSenderId: "853950108531",
-  appId: "1:853950108531:web:0b70afd3d3d71addc2eb06",
-  measurementId: "G-B4741RFE19",
+    apiKey: "AIzaSyAv-qdHJTtM3PZleVXA8ZuVlGnHLYjcUJs",
+    authDomain: "teampage-3217c.firebaseapp.com",
+    projectId: "teampage-3217c",
+    storageBucket: "teampage-3217c.appspot.com",
+    messagingSenderId: "853950108531",
+    appId: "1:853950108531:web:0b70afd3d3d71addc2eb06",
+    measurementId: "G-B4741RFE19",
 };
 
 // Firebase 인스턴스 초기화
@@ -31,26 +31,46 @@ const db = getFirestore(app);
 
 /************************   Icons  ************************/
 $(document).on("click", "#naver", () => {
-  window.location.href = "https://naver.com";
+    window.location.href = "https://naver.com";
 });
 $(document).on("click", "#github", () => {
-  window.location.href = "https://github.com";
+    window.location.href = "https://github.com";
 });
 $(document).on("click", "#notion", () => {
-  window.location.href = "https://notion.so";
+    window.location.href = "https://notion.so";
 });
 $(document).on("click", "#google", () => {
-  window.location.href = "https://google.com";
+    window.location.href = "https://google.com";
+});
+
+/************************   NavBars  ************************/
+
+//하드코딩 식이라 이 부분 수정 필요
+$('#teamContent').show();
+
+// 모든 메뉴 링크에 클릭 이벤트 추가
+$('.nav-link').click(function (event) {
+    // 기본 동작 방지
+    event.preventDefault();
+
+    // 모든 컨텐츠 숨기기
+    $('.content').hide();
+
+    // 클릭된 링크의 부모 요소의 id 가져오기
+    var targetId = $(this).parent().attr('id') + 'Content';
+
+    // 해당하는 컨텐츠 보이기
+    $('#' + targetId).show();
 });
 
 /************************   Create  ************************/
 // '멤버 추가' 버튼 클릭시
 $(document).on("click", "#create-button", async function () {
-  window.open(
-    "newcard.html",
-    "newCard",
-    "width=520,height=670,left=200,top=100"
-  );
+    window.open(
+        "newcard.html",
+        "newCard",
+        "width=520,height=670,left=200,top=100"
+    );
 });
 
 let fileDOM = document.querySelector("#file");
@@ -58,92 +78,92 @@ let preview = document.querySelector("#image-box");
 
 // 이미지 파일 업로드 함수
 function getImage() {
-  const reader = new FileReader();
-  //FileReader가 파일 읽기를 완료(onload)하면,
-  reader.onload = ({ target }) => {
-    //읽어들인 파일의 데이터 url을 preview의 src 속성에 넣어줍니다.
-    preview.src = target.result;
-  };
-  //업로드한 이미지 파일의 url을 읽습니다.
-  reader.readAsDataURL(fileDOM.files[0]);
+    const reader = new FileReader();
+    //FileReader가 파일 읽기를 완료(onload)하면,
+    reader.onload = ({ target }) => {
+        //읽어들인 파일의 데이터 url을 preview의 src 속성에 넣어줍니다.
+        preview.src = target.result;
+    };
+    //업로드한 이미지 파일의 url을 읽습니다.
+    reader.readAsDataURL(fileDOM.files[0]);
 }
 // 'Upload image' 버튼 클릭하여 새 파일을 선택하면
 fileDOM?.addEventListener("change", function () {
-  //getImage 함수를 실행하여 선택한 이미지 파일의 데이터 url을 이미지 preview 요소의 src에 넣어줍니다.
-  getImage();
+    //getImage 함수를 실행하여 선택한 이미지 파일의 데이터 url을 이미지 preview 요소의 src에 넣어줍니다.
+    getImage();
 });
 
 // 팝업창 '추가' 버튼 클릭시
 $(document).on("click", "#add-button", async function () {
-  //비밀번호 입력란에 입력한 글자가 4자리 숫자인 경우
-  if (/^\d{4}$/.test($("#password").val())) {
-    //Firestore에 저장할 doc을 생성합니다.
-    let doc = {
-      image: $("#image-box").prop("src"),
-      name: $("#name").val(),
-      introduce: $("#introduce").val(),
-      mbti: $("#mbti").val(),
-      time: Timestamp.now(),
-      pw: $("#password").val(),
-    };
-    //입력한 멤버의 데이터를 Firestore에 저장합니다.
-    await addDoc(collection(db, "members"), doc);
-    alert("저장 완료!");
-    //팝업창을 닫습니다.
-    self.close();
-    //메인창을 새로고침 합니다.
-    window.opener.location.reload();
-  } else {
-    //비밀번호 입력란에 입력한 글자가 4자리 숫자가 아닌 경우
-    alert("4자리 숫자로 입력해주세요.");
-  }
+    //비밀번호 입력란에 입력한 글자가 4자리 숫자인 경우
+    if (/^\d{4}$/.test($("#password").val())) {
+        //Firestore에 저장할 doc을 생성합니다.
+        let doc = {
+            image: $("#image-box").prop("src"),
+            name: $("#name").val(),
+            introduce: $("#introduce").val(),
+            mbti: $("#mbti").val(),
+            time: Timestamp.now(),
+            pw: $("#password").val(),
+        };
+        //입력한 멤버의 데이터를 Firestore에 저장합니다.
+        await addDoc(collection(db, "members"), doc);
+        alert("저장 완료!");
+        //팝업창을 닫습니다.
+        self.close();
+        //메인창을 새로고침 합니다.
+        window.opener.location.reload();
+    } else {
+        //비밀번호 입력란에 입력한 글자가 4자리 숫자가 아닌 경우
+        alert("4자리 숫자로 입력해주세요.");
+    }
 });
 
 // 팝업창 '닫기' 버튼 클릭시
 $(document).on("click", "#close-button", async function () {
-  //팝업창을 닫습니다.
-  self.close();
+    //팝업창을 닫습니다.
+    self.close();
 });
 
 // teambtn을 클릭하면 새로운 이미지를 표시하도록 이벤트를 추가합니다.
 $(document).on("click", ".teambtn", function () {
-  // 기존의 멤버 카드를 삭제합니다.
-  $("#card").empty();
+    // 기존의 멤버 카드를 삭제합니다.
+    $("#card").empty();
 
-  // 새 이미지의 URL 주소입니다.
-  let imageURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgsklthp1tSNy-mnv-r6mmcUcLaRe8eF05Hg&s";
+    // 새 이미지의 URL 주소입니다.
+    let imageURL =
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgsklthp1tSNy-mnv-r6mmcUcLaRe8eF05Hg&s";
 
-  // 새 이미지를 만듭니다.
-  let newImage = $("<img>");
-  newImage.attr("src", imageURL); // 이미지의 URL을 설정합니다.
-  newImage.attr("alt", "New Image");
+    // 새 이미지를 만듭니다.
+    let newImage = $("<img>");
+    newImage.attr("src", imageURL); // 이미지의 URL을 설정합니다.
+    newImage.attr("alt", "New Image");
 
-  // 새 이미지를 페이지에 추가합니다.
-  $("#card").append(newImage);
+    // 새 이미지를 페이지에 추가합니다.
+    $("#card").append(newImage);
 });
 
 // memberbtn을 클릭하면 기존의 index.html 페이지로 이동하도록 이벤트를 추가합니다.
 $(document).on("click", ".memberbtn", function () {
-  window.location.href = "index.html";
+    window.location.href = "index.html";
 });
 
 /************************   Read  ************************/
 let docs = await getDocs(
-  //시간순 정렬해서 query로 불러오기
-  query(collection(db, "members"), orderBy("time"))
+    //시간순 정렬해서 query로 불러오기
+    query(collection(db, "members"), orderBy("time"))
 );
 
 async function readDB() {
-  docs.forEach((doc) => {
-    let row = doc.data();
-    let image = row["image"];
-    let name = row["name"];
-    let introduce = row["introduce"];
-    let mbti = row["mbti"];
+    docs.forEach((doc) => {
+        let row = doc.data();
+        let image = row["image"];
+        let name = row["name"];
+        let introduce = row["introduce"];
+        let mbti = row["mbti"];
 
-    //불러올 멤버카드 템플릿
-    let temp_html = `
+        //불러올 멤버카드 템플릿
+        let temp_html = `
           <div class="col">
             <div class="card h-100">
               <img
@@ -164,124 +184,124 @@ async function readDB() {
               </div>
             </div>
           </div>`;
-    //카드 목록에 추가
-    $("#card").append(temp_html);
-  });
+        //카드 목록에 추가
+        $("#card").append(temp_html);
+    });
 }
 
 // 이미지 클릭시 새창으로 개인 페이지 열기
 $(document).on("click", "#member-image", function () {
-  window.open(
-    "member.html",
-    "member-image",
-    "width=500,height=500,left=200,top=200"
-  );
+    window.open(
+        "member.html",
+        "member-image",
+        "width=500,height=500,left=200,top=200"
+    );
 });
 
 /************************   Update  ************************/
 // '수정' 버튼 클릭시
 $(document).on("click", "#edit-button", async function () {
-  //클릭된 버튼의 부모 요소, 즉 div class="mybuttons"에서 멤버의 ID를 가져옵니다.
-  let docID = this.parentElement.id;
-  //Firestore에서 해당 멤버의 데이터를 가져옵니다.
-  let memberDoc = await getDoc(doc(db, "members", docID));
-  let memberData = memberDoc.data();
-  //비밀번호 확인 프롬프트 창을 엽니다.
-  let inputPw = window.prompt("비밀번호를 입력해주세요.", "숫자 4자리");
-  //프롬프트에 입력한 글자가 4자리 숫자인 경우
-  if (/^\d{4}$/.test(inputPw)) {
-    //프롬프트에 입력한 4자리 숫자가 비밀번호와 일치하는 경우
-    if (inputPw == memberData["pw"]) {
-      let openWin = window.open(
-        "newcard.html",
-        "updateCard",
-        "width=520,height=670,left=200,top=100"
-      );
-      //새 창을 열고 수정 전 데이터를 입력창에 표시합니다.
-      openWin.onload = function () {
-        openWin.document.getElementById("image-box").src = memberData["image"];
-        openWin.document.getElementById("name").value = memberData["name"];
-        openWin.document.getElementById("introduce").value =
-          memberData["introduce"];
-        openWin.document.getElementById("mbti").value = memberData["mbti"];
-        openWin.document.getElementById("password").value = memberData["pw"];
-        openWin.document.getElementById("add-button").textContent = "확인";
-        openWin.document.getElementById("add-button").value = docID;
-        openWin.document.getElementById("add-button").id = "confirm-button";
-      };
+    //클릭된 버튼의 부모 요소, 즉 div class="mybuttons"에서 멤버의 ID를 가져옵니다.
+    let docID = this.parentElement.id;
+    //Firestore에서 해당 멤버의 데이터를 가져옵니다.
+    let memberDoc = await getDoc(doc(db, "members", docID));
+    let memberData = memberDoc.data();
+    //비밀번호 확인 프롬프트 창을 엽니다.
+    let inputPw = window.prompt("비밀번호를 입력해주세요.", "숫자 4자리");
+    //프롬프트에 입력한 글자가 4자리 숫자인 경우
+    if (/^\d{4}$/.test(inputPw)) {
+        //프롬프트에 입력한 4자리 숫자가 비밀번호와 일치하는 경우
+        if (inputPw == memberData["pw"]) {
+            let openWin = window.open(
+                "newcard.html",
+                "updateCard",
+                "width=520,height=670,left=200,top=100"
+            );
+            //새 창을 열고 수정 전 데이터를 입력창에 표시합니다.
+            openWin.onload = function () {
+                openWin.document.getElementById("image-box").src = memberData["image"];
+                openWin.document.getElementById("name").value = memberData["name"];
+                openWin.document.getElementById("introduce").value =
+                    memberData["introduce"];
+                openWin.document.getElementById("mbti").value = memberData["mbti"];
+                openWin.document.getElementById("password").value = memberData["pw"];
+                openWin.document.getElementById("add-button").textContent = "확인";
+                openWin.document.getElementById("add-button").value = docID;
+                openWin.document.getElementById("add-button").id = "confirm-button";
+            };
+        } else {
+            //프롬프트에 입력한 4자리 숫자가 비밀번호와 일치하는지 않는 경우
+            alert("비밀번호가 일치하지 않습니다.");
+        }
     } else {
-      //프롬프트에 입력한 4자리 숫자가 비밀번호와 일치하는지 않는 경우
-      alert("비밀번호가 일치하지 않습니다.");
+        //프롬프트에 입력한 글자가 4자리 숫자가 아닌 경우
+        alert("4자리 숫자로 입력해주세요.");
     }
-  } else {
-    //프롬프트에 입력한 글자가 4자리 숫자가 아닌 경우
-    alert("4자리 숫자로 입력해주세요.");
-  }
 });
 
 // '확인' 버튼 클릭시
 $(document).on("click", "#confirm-button", async function () {
-  // 클릭된 버튼의 value 값에서 멤버카드 데이터의 ID를 가져옵니다.
-  let docID = this.value;
-  //프롬프트에 입력한 글자가 4자리 숫자인 경우
-  if (/^\d{4}$/.test($("#password").val())) {
-    // Firestore에서 해당 멤버의 데이터를 입력창에 적힌 값으로 업데이트 합니다.
-    await updateDoc(doc(db, "members", docID), {
-      image: $("#image-box").prop("src"),
-      name: $("#name").val(),
-      introduce: $("#introduce").val(),
-      mbti: $("#mbti").val(),
-      pw: $("#password").val(),
-    });
-    alert("수정 완료!");
-    //팝업 창을 닫습니다.
-    self.close();
-    //메인 창을 새로고침 합니다.
-    window.opener.location.reload();
-  } else {
-    //프롬프트에 입력한 글자가 4자리 숫자가 아닌 경우
-    alert("4자리 숫자로 입력해주세요.");
-  }
+    // 클릭된 버튼의 value 값에서 멤버카드 데이터의 ID를 가져옵니다.
+    let docID = this.value;
+    //프롬프트에 입력한 글자가 4자리 숫자인 경우
+    if (/^\d{4}$/.test($("#password").val())) {
+        // Firestore에서 해당 멤버의 데이터를 입력창에 적힌 값으로 업데이트 합니다.
+        await updateDoc(doc(db, "members", docID), {
+            image: $("#image-box").prop("src"),
+            name: $("#name").val(),
+            introduce: $("#introduce").val(),
+            mbti: $("#mbti").val(),
+            pw: $("#password").val(),
+        });
+        alert("수정 완료!");
+        //팝업 창을 닫습니다.
+        self.close();
+        //메인 창을 새로고침 합니다.
+        window.opener.location.reload();
+    } else {
+        //프롬프트에 입력한 글자가 4자리 숫자가 아닌 경우
+        alert("4자리 숫자로 입력해주세요.");
+    }
 });
 
 /************************   Delete  ************************/
 // '삭제' 버튼 클릭시
 $(document).on("click", "#delete-button", async function () {
-  //클릭된 버튼의 부모 요소, 즉 div class="mybuttons"에서 멤버의 ID를 가져옵니다.
-  let docID = this.parentElement.id;
-  //Firestore에서 해당 멤버의 데이터를 가져옵니다.
-  let memberDoc = await getDoc(doc(db, "members", docID));
-  let memberData = memberDoc.data();
-  //비밀번호 확인 프롬프트 창을 엽니다.
-  let inputPw = window.prompt("비밀번호를 입력해주세요.", "숫자 4자리");
-  //프롬프트에 입력한 글자가 4자리 숫자인 경우
-  if (/^\d{4}$/.test(inputPw)) {
-    //프롬프트에 입력한 4자리 숫자가 비밀번호와 일치하는 경우
-    if (inputPw == memberData["pw"]) {
-      // Firestore에서 해당 멤버의 데이터를 삭제합니다.
-      await deleteDoc(doc(db, "members", docID));
-      alert("삭제되었습니다.");
-      window.location.reload();
+    //클릭된 버튼의 부모 요소, 즉 div class="mybuttons"에서 멤버의 ID를 가져옵니다.
+    let docID = this.parentElement.id;
+    //Firestore에서 해당 멤버의 데이터를 가져옵니다.
+    let memberDoc = await getDoc(doc(db, "members", docID));
+    let memberData = memberDoc.data();
+    //비밀번호 확인 프롬프트 창을 엽니다.
+    let inputPw = window.prompt("비밀번호를 입력해주세요.", "숫자 4자리");
+    //프롬프트에 입력한 글자가 4자리 숫자인 경우
+    if (/^\d{4}$/.test(inputPw)) {
+        //프롬프트에 입력한 4자리 숫자가 비밀번호와 일치하는 경우
+        if (inputPw == memberData["pw"]) {
+            // Firestore에서 해당 멤버의 데이터를 삭제합니다.
+            await deleteDoc(doc(db, "members", docID));
+            alert("삭제되었습니다.");
+            window.location.reload();
+        } else {
+            //프롬프트에 입력한 4자리 숫자가 비밀번호와 일치하는지 않는 경우
+            alert("비밀번호가 일치하지 않습니다.");
+        }
     } else {
-      //프롬프트에 입력한 4자리 숫자가 비밀번호와 일치하는지 않는 경우
-      alert("비밀번호가 일치하지 않습니다.");
+        //프롬프트에 입력한 글자가 4자리 숫자가 아닌 경우
+        alert("4자리 숫자로 입력해주세요.");
     }
-  } else {
-    //프롬프트에 입력한 글자가 4자리 숫자가 아닌 경우
-    alert("4자리 숫자로 입력해주세요.");
-  }
 });
 
 /************************   Initiate  ************************/
 function initPage() {
-  //문서의 현재 상태가 로딩 중이 아니라면
-  if (document.readyState !== "loading") {
-    //바로 readDB 함수를 사용해 저장된 데이터를 불러와 웹페이지에 표시합니다.
-    readDB();
-  } else {
-    //로딩 중이라면 DOM 컨텐츠 로딩을 모두 완료한 후, readDB 함수를 실행합니다.
-    document.addEventListener("DOMContentLoaded", readDB);
-  }
+    //문서의 현재 상태가 로딩 중이 아니라면
+    if (document.readyState !== "loading") {
+        //바로 readDB 함수를 사용해 저장된 데이터를 불러와 웹페이지에 표시합니다.
+        readDB();
+    } else {
+        //로딩 중이라면 DOM 컨텐츠 로딩을 모두 완료한 후, readDB 함수를 실행합니다.
+        document.addEventListener("DOMContentLoaded", readDB);
+    }
 }
 // initPage 함수를 사용해 웹페이지가 새로고침 되었을 때
 initPage();
